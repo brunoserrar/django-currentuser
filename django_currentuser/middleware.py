cf5e427a51d69063051b4d86017aa72601d4fadf
brunoserra <brunoserrabr@gmail.com>
2025-03-21 20:20:02 -0300

    asgi

--- django_currentuser/middleware.py
+++ django_currentuser/middleware.py
@@ -1,11 +1,12 @@
 from django.conf import settings
 from django.contrib.auth.models import AnonymousUser
-from threading import local
+#from threading import local
+from asgiref.local import Local
 
 USER_ATTR_NAME = getattr(settings, 'LOCAL_USER_ATTR_NAME', '_current_user')
 
-_thread_locals = local()
-
+#_thread_locals = local()
+_thread_locals = Local()
 
 def _do_set_current_user(user_fun):
     setattr(_thread_locals, USER_ATTR_NAME, user_fun.__get__(user_fun, local))
